f22aba6d2671f898163090e8fb46fe5f62fb9293
Unix One <unix1@live.com>
2018-11-11 15:21:45 -0800

    Update copyright notices

--- augmentedtree/interface.go
+++ augmentedtree/interface.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2014 Workiva, LLC
+Copyright 2018 Packet Clearing House
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
